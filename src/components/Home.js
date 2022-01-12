de3a4a39eed0d5008852654db50ffc88e53e3e8b
oyindamola <oyindamoladawodu22@gmail.com>
2022-01-12 12:56:21 +0100

    local storage feature added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,148 +4,94 @@ import Nav from './Nav';
 import Footer from './Footer';
 
 const Home = () => {
+    const saved = localStorage.getItem("likes");
+    const initialValue = JSON.parse(saved) || "";
+    let newLikes = [];
+    newLikes.push(...initialValue)
 
-  const [data, setData] = useState([]);
-  const [loading, setloading] = useState(true);
-  const [AllLikes, setAllLikes] = useState([]);
-  const [liked, setLiked] = useState(false);
-  const [nasaKey, setNasaKey] = useState ([])
-
-  const newInput = JSON.parse(JSON.stringify(AllLikes));
+    // ALL STATES
+    const [data, setData] = useState([]);
+    const [loading, setloading] = useState(true);
+    const [toggle, setToggle] = useState(false);
 
   
-  useEffect(() => {
-    const fetchData = async () => {
 
-      try {
-        const result = await axios(
-          'https://api.nasa.gov/techtransfer/patent/?engine&api_key=nKrFP0XDzSRf9a6xPIPaGjr2Jxt4hFqiabyRrAoS',
-        );
-        const items = result.data.results;
-        setData(items);
-        setloading(false);
-      
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    const fetchLikes = async () => {
-      const data = await localStorage.getItem("likes");
-      if (data) {
-        setAllLikes(JSON.parse(data));
-
-        for (let i = 0; i < data.length; i++){
-          nasaKey.push(data[i].id)
-        }
-        console.log(nasaKey); 
-     //   and for each element set their isclick state to true
-        console.log(AllLikes);
-      }
-     
-    } 
-   
-    fetchData()
-    fetchLikes()
-    localStorage.setItem("likes", JSON.stringify(AllLikes));
-   
-  }, []);
-    
-
-  const handleToggle = (e) => {
-    const { id } = e.currentTarget;
-    const heart = document.getElementById(id);
-
-    const check = AllLikes.find((element, index) => {
-      return element.id === id
-    })
-
-    setNasaKey(prevState => {
-      return {
-        ...prevState,
-        check
-      }
-    })
-
-   
-  }
-  
-
-  // const handleToggle = (e) => {
-  //   const { id } = e.currentTarget;
-  //   const heart = document.getElementById(id);
-  //     // const sound = document.getElementById("audio");
-  //     setClick(!isClick)
-  //     if (isClick === true) {
-  //       heart.classList.add("animated-heart");
-  //       // sound.play();
-  //       newInput.push({
-  //         id: id,
-  //         liked: isClick
-  //       });
-      
-  //       setLikes(newInput)
-  //     } else if (isClick === false) {
-  //       heart.classList.remove("animated-heart");
-  //       // sound.pause();
-  //       // sound.currentTime = 0;
-  //       setLikes((prevState) => {
-  //         return [...prevState.filter((like) => like.id !== id)];
-  //       });
-  //   }
- 
-  // }
-
-
- 
-  // const style = () => {
-  //   const stats = likes.map((item) => item.liked);
-  //   console.log(stats);
-  //   if (stats) {
-  //        document.getElementById(id).classList.add("animated-heart");
-  //   }
-    
-  //   // ids.forEach((i) => {
-  //   //   document.getElementById(i).classList.add("animated-heart");
-  //   // })
-  //   // console.log(ids);
-  // }
-
-
-  return (
-    <>
-    <Nav/>
-      <p className="intro">NASA's Technology Transfer Program ensures that innovations developed for exploration and discovery are broadly available to the public. The NASA patent portfolio is available to benefit US citizens. Through partnerships and licensing agreements with industry, these patents ensure that NASA’s investments in pioneering research find secondary uses that benefit the economy, create jobs, and improve quality of life. This endpoint provides structured, searchable developer access to NASA’s patents, software, and technology spinoff descriptions that have been curated to support technology transfer. More information can be found at <a href="https://technology.nasa.gov/">technology.nasa.gov</a> and <a href="https://software.nasa.gov/">software.nasa.gov</a> and <a href="https://spinoff.nasa.gov/">spinoff.nasa.gov</a>.</p>
-      
-      <p className="intro">Some Examples are:</p>
-      {loading ? <div className="loading"><img src="/loading.gif" alt="loading gif" /></div> : ""}
-        <div className="data-container">
-          {data.map((dataItem, index) => {
-          return (
-            <div key={dataItem[0]}  className="data-box" >
-              <div className="title">
-              <h1 dangerouslySetInnerHTML={{ __html: dataItem[2] }}></h1>
-              <p> ({dataItem[5]})</p>
-              </div>
-              <div className="flex-box">
-                <p>{dataItem[1]}</p>
-                <p>{dataItem[12]}</p>
-              </div>
-              <img src={dataItem[10]} alt="nasa-images" className="nasa-images" />
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axios(
+                    'https://api.nasa.gov/techtransfer/patent/?engine&api_key=nKrFP0XDzSRf9a6xPIPaGjr2Jxt4hFqiabyRrAoS',
+                );
+                const items = result.data.results;
+                setData(items);
+                setloading(false);
               
-                  <span
-                    key={dataItem[0]} id={dataItem[0]} className=" Heart" onClick={handleToggle}>
-                    {nasaKey.includes(dataItem[0]) ? 'y' : 'n'}
-                  </span>
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData();     
+    }, []);
+
+    const handleToggle = (e) => {
+        const { id } = e.currentTarget;
+        const heart = document.getElementById(id);
+
+        setToggle(!toggle);
+
+       newLikes = [...initialValue, {
+            key: id,
+            liked: toggle
+        }]
+        
+        if (toggle) {
+            heart.classList.add("animated-heart"); 
+        } else {
+            heart.classList.remove("animated-heart");
+            
+            newLikes = [...newLikes.filter((like) => like.key !== id)];
+        }
+        
+        localStorage.setItem("likes", JSON.stringify(newLikes))
+    }
+    
+    return (
+        <>
+                <Nav />
+                <p className="intro">NASA's Technology Transfer Program ensures that innovations developed for exploration and discovery are broadly available to the public. The NASA patent portfolio is available to benefit US citizens. Through partnerships and licensing agreements with industry, these patents ensure that NASA’s investments in pioneering research find secondary uses that benefit the economy, create jobs, and improve quality of life. This endpoint provides structured, searchable developer access to NASA’s patents, software, and technology spinoff descriptions that have been curated to support technology transfer. More information can be found at <a href="https://technology.nasa.gov/">technology.nasa.gov</a> and <a href="https://software.nasa.gov/">software.nasa.gov</a> and <a href="https://spinoff.nasa.gov/">spinoff.nasa.gov</a>.</p>
+      
+                <p className="intro">Some Examples are:</p>
+            {loading ? <div className="loading"><img src="/loading.gif" alt="loading gif" /></div> : ""}
+            
+                <div className="data-container">
+                {data.map((dataItem, index) => {
+                    const check = newLikes.some(e => e.key === dataItem[0])
                 
-          </div>
-          )
-            })}
-      </div>
-      <audio id="audio" src="/TEMISAN.mp3" autoplay="false" ></audio>
+                        return (
+                            <div key={dataItem[0]} className="data-box" >
+                                <div className="title">
+                                    <h1 dangerouslySetInnerHTML={{ __html: dataItem[2] }}></h1>
+                                    <p> ({dataItem[5]})</p>
+                                </div>
+                                <div className="flex-box">
+                                    <p>{dataItem[1]}</p>
+                                    <p>{dataItem[12]}</p>
+                                </div>
+                                <img src={dataItem[10]} alt="nasa-images" className="nasa-images" />  
+                                
+                                <span
+                                    key={dataItem[0]} id={dataItem[0]} className={check ? "animated-heart" : "Heart"} onClick= {handleToggle}  >
+                                    ❤ 
+                                </span>
+                            </div>
+                        )
+                    })}
+                </div>
   
-      <Footer/>
-    </>
-    )
+                <Footer />
+            </>
+        )
+    
 }
 
 export default Home;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <div class="nav-container">
+        <div className="nav-container">
             <nav>
                 <a href="/" className="brand"><img id="logo" src="/tech-transfer.png" alt="logo" />
                 <h1 >TechTransfer</h1> </a>   
